56cee2ace5447801cc99fd26d898f601e972a06c
beer_siri69 <beer_siri69@yahoo.co.th>
2021-05-07 10:43:53 +0700

    Change Sidebar

--- src/app/Layouts/sidebar/sidebar.component.ts
+++ src/app/Layouts/sidebar/sidebar.component.ts
@@ -8,13 +8,12 @@ declare interface RouteInfo {
     class: string;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Dashboard',  icon: 'ni-tv-2 text-primary', class: '' },
-    { path: '/icons', title: 'Icons',  icon:'ni-planet text-blue', class: '' },
-    { path: '/maps', title: 'Maps',  icon:'ni-pin-3 text-orange', class: '' },
+    { path: '/dashboard', title: 'Operation Dashboard',  icon: 'ni-tv-2 text-primary', class: '' },
+    { path: '/icons', title: 'Manager Dashboard',  icon:'ni ni-chart-bar-32 text-blue', class: '' },
+    { path: '/maps', title: 'Financial Dashboard',  icon:'ni-money-coins text-orange', class: '' },
+    { path: '/tables', title: 'Search',  icon:'ni fas fa-search text-red', class: '' },
     { path: '/user-profile', title: 'User profile',  icon:'ni-single-02 text-yellow', class: '' },
-    { path: '/tables', title: 'Tables',  icon:'ni-bullet-list-67 text-red', class: '' },
-    { path: '/login', title: 'Login',  icon:'ni-key-25 text-info', class: '' },
-    { path: '/register', title: 'Register',  icon:'ni-circle-08 text-pink', class: '' }
+    { path: '/login', title: 'Logout',  icon:'ni-button-power text-info', class: '' },
 ];
 
 @Component({
