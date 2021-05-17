78cd4a39ee78bad9c406bb230d0721cd0dfe050d
Ganjagorn Piromrat <ganjagorn.p@mail.kmutt.ac.th>
2021-05-18 06:53:46 +0700

    add package log

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { from } from 'rxjs';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule, } from '@angular/common';
-
+import { ChartModule  } from 'angular2-chartjs';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -11,64 +11,40 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule,  } from '@angular/router';
 import { ComponentsModule } from './Layouts/components.module'
 
-import {  HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AuthTemplateModule } from './Template/auth-template/auth-template.module';
-import { SearchModule } from './Pages/search/search.module';
+
 import { SearchComponent } from './Pages/search/search.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { ChartsModule } from 'ng2-charts';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-<<<<<<< Updated upstream
-    SearchComponent,    
-  ],
-  imports: [ 
-=======
-<<<<<<< Updated upstream
-    FooterComponent,
-    SidebarComponent,
-    NavbarComponent,
-    HomeComponent,
-    OpDashboardComponent,
-    MnDashboardComponent,
-    SearchComponent,
-    GeneralJournalComponent,
 
-  ],
-  imports: [
-=======
-    SearchComponent,    
+    AppComponent,
+    SearchComponent,
+  
   ],
   imports: [ 
     AppRoutingModule,
    
-    
-    //This module call by app module
+    //SearchModule,
     AuthTemplateModule,
-    ChartsModule,
-    CommonModule,
-    RouterModule,
-    BrowserModule,
-    AppRoutingModule,
-   
-    
-    //This module call by app module
-    AuthTemplateModule,
-    ChartsModule,
+
     CommonModule,
     RouterModule,
     BrowserModule,
   
     NgbModule,
-
+    ChartModule,   
     ComponentsModule,    
     HttpClientModule, BrowserAnimationsModule,
     
+    FormsModule,
+    ReactiveFormsModule
    
   ],
   providers: [],
